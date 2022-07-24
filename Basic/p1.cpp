238c9336066fa1da49e1f22d838d6bd937d7e5a4
marufmarzuq <marufmarzuq18@gmail.com>
2022-07-24 09:51:45 +0600

    first commit

---
+++ Basic/p1.cpp
@@ -0,0 +1,17 @@
+/* Task1: একটি প্রোগ্রাম লিখুন যা এসি চালু করতে নির্দেশ দেবে যদি পরিবেশের তাপমাত্রা 25 ডিগ্রির বেশি হয়। আবার 18
+ডিগ্রির কম তাপমাত্রা হলে এসি বন্ধ করার নির্দেশ দেবে। */
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+// 	int temp;
+// 	cin>>temp;
+	
+// 	if(temp > 29) { cout<< "Turn on AC"; }
+// 	else if(temp < 18) { cout<< "Turn off AC"; }
+// 	else cout<< "Let the AC on";
+
+// 	return 0;
+// }
---
+++ Practice/hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num = 75;
+    char charecter = 'a';
+
+    if(num > 80 ) {
+    cout << num << "True" << endl;
+    }
+    else {
+         cout << num << "false" << endl;
+    }
+
+    return 0;
+}
